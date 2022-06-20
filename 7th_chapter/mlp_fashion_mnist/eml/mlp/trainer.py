fd648362723189554dd250714a0e168f74dbc8db
Walter Ehrenberger <wehrenberger@salesforce.com>
2022-06-20 15:53:01 +0200

    Implemented 7.2

--- 7th_chapter/mlp_fashion_mnist/eml/mlp/trainer.py
+++ 7th_chapter/mlp_fashion_mnist/eml/mlp/trainer.py
@@ -1,27 +1,40 @@
-## Trains the given MLP-model.
+# Trains the given MLP-model.
 #  @param i_loss_func used loss function.
 #  @param io_data_loader data loader containing the data to which the model is applied (single epoch).
 #  @param io_model model which is trained.
 #  @param io_optimizer.
 #  @return summed loss over all training samples.
-def train( i_loss_func,
-           io_data_loader,
-           io_model,
-           io_optimizer ):
-  # switch model to training mode
-  io_model.train()
+import torch
+import torch.distributed
 
-  l_loss_total = 0
 
-  for l_batch_id, (l_x, l_y) in enumerate( io_data_loader ):
-    # compute prediction and loss
-    l_prediction = io_model( l_x )
-    l_loss = i_loss_func( l_prediction, l_y )
-    l_loss_total += l_loss.item()
+def train(i_loss_func,
+          i_size,
+          io_data_loader,
+          io_model,
+          io_optimizer):
+    # switch model to training mode
+    io_model.train()
 
-    # backprop
-    io_optimizer.zero_grad()
-    l_loss.backward()
-    io_optimizer.step()
+    l_loss_total = 0
 
-  return l_loss_total
+    for l_batch_id, (l_x, l_y) in enumerate(io_data_loader):
+        # compute prediction and loss
+        l_prediction = io_model(l_x)
+        l_loss = i_loss_func(l_prediction, l_y)
+        l_loss_total += l_loss.item()
+
+        # backprop
+        io_optimizer.zero_grad()
+        l_loss.backward()
+        # ------------------- 7.3 Addon ------------------- #
+        # Gradienten mit All reduce syncen und mitteln
+
+        for l_param in io_model.parameters():
+            torch.distributed.all_reduce(l_param.grad_data,
+                                         op=torch.distributed.ReduceOp.SUM)
+        l_param.grad_data - l_param.grad_data / float(i_size)
+        # ------------------------------------------------- #
+        io_optimizer.step()
+
+    return l_loss_total
--- 7th_chapter/mlp_fashion_mnist/mlp_fashion_mnist.py
+++ 7th_chapter/mlp_fashion_mnist/mlp_fashion_mnist.py
@@ -1,82 +1,150 @@
 #!/usr/bin/python3
+import sys
+
 import torch
 import torchvision.datasets
 import torchvision.transforms
 import torch.utils.data
+import torch.distributed
 
 import eml.mlp.model
 import eml.mlp.trainer
 import eml.mlp.tester
-import eml.vis.fashion_mnist
+# import eml.vis.fashion_mnist
 
-print( "################################" )
-print( "# Welcome to EML's MLP example #" )
-print( "################################" )
+print("################################")
+print("# Welcome to EML's MLP example #")
+print("################################")
+
+# ------------------- 7.2 Addon ------------------- #
+# Init MPI
+torch.distributed.init_process_group('mpi')
+l_rank = torch.distributed.get_rank()
+l_size = torch.distributed.get_world_size()
+
+# cheesy overload
+
+
+def print(msg):
+    if l_rank == 0:
+        print(msg, file=sys.stdout)
+# ------------------------------------------------- #
 
 
 # set up datasets
-print( 'setting up datasets')
-l_data_train = torchvision.datasets.FashionMNIST( root      = "data/fashion_mnist",
-                                                  train     = True,
-                                                  download  = True,
-                                                  transform = torchvision.transforms.ToTensor() )
+print('setting up datasets')
+l_data_train = torchvision.datasets.FashionMNIST(root="data/fashion_mnist",
+                                                 train=True,
+                                                 download=True,
+                                                 transform=torchvision.transforms.ToTensor())
 
-l_data_test = torchvision.datasets.FashionMNIST( root      = "data/fashion_mnist",
-                                                 train     = False,
-                                                 download  = True,
-                                                 transform = torchvision.transforms.ToTensor() )
+l_data_test = torchvision.datasets.FashionMNIST(root="data/fashion_mnist",
+                                                train=False,
+                                                download=True,
+                                                transform=torchvision.transforms.ToTensor())
+
+# ------------------- 7.2 Addon ------------------- #
+# 2 distributed data sampler for train and test data
+
+BATCH_SIZE = 64  # same as mini batch size
+MICRO_BATCH_SIZE = BATCH_SIZE // l_size
+
+# @num_replicas, number of running nodes
+# @rank, index of current node
+# @shuffle, shuffle data, same for all nodes
+# @drop_last, drops last batch if smaller than batch_size
+
+l_dist_sampler_train = torch.utils.data.DistributedSampler(
+    l_data_train,
+    num_replicas=l_size,
+    rank=l_rank,
+    shuffle=False,
+    drop_last=False
+)
+
+l_dist_sampler_test = torch.utils.data.DistributedSampler(
+    l_data_test,
+    num_replicas=l_size,
+    rank=l_rank,
+    shuffle=False,
+    drop_last=False
+)
+
+# 2 batch sampler for train and test data with micro batch size
+
+# @batch_size, we use our micro batch size for the nodes
+# @drop_last, drops last batch if smaller than micro_batch_size
+
+l_batch_sampler_train = torch.utils.data.BatchSampler(
+    sampler=l_dist_sampler_train,
+    batch_size=MICRO_BATCH_SIZE,
+    drop_last=False
+)
+
+l_batch_sampler_test = torch.utils.data.BatchSampler(
+    sampler=l_dist_sampler_test,
+    batch_size=MICRO_BATCH_SIZE,
+    drop_last=False
+)
 
 # init data loaders
-print( 'initializing data loaders' )
-l_data_loader_train = torch.utils.data.DataLoader( l_data_train,
-                                                   batch_size = 64 )
-l_data_loader_test  = torch.utils.data.DataLoader( l_data_test,
-                                                   batch_size = 64 )
+print('initializing data loaders, ')
+l_data_loader_train = torch.utils.data.DataLoader(l_data_train,
+                                                  batch_sampler=l_batch_sampler_train)
+l_data_loader_test = torch.utils.data.DataLoader(l_data_test,
+                                                 batch_sampler=l_batch_sampler_test)
+# ------------------------------------------------- #
 
 # set up model, loss function and optimizer
-print( 'setting up model, loss function and optimizer' )
+print('setting up model, loss function and optimizer')
 l_model = eml.mlp.model.Model()
 l_loss_func = torch.nn.CrossEntropyLoss()
-l_optimizer = torch.optim.SGD( l_model.parameters(),
-                               lr = 1E-3 )
-print( l_model )
+l_optimizer = torch.optim.SGD(l_model.parameters(),
+                              lr=1E-3)
+print(l_model)
 
 # train for the given number of epochs
+# For training loss and test with all reduce
 l_n_epochs = 25
-for l_epoch in range( l_n_epochs ):
-  print( 'training epoch #' + str(l_epoch+1) )
-  l_loss_train = eml.mlp.trainer.train( l_loss_func,
-                                        l_data_loader_train,
-                                        l_model,
-                                        l_optimizer )
-  print( '  training loss:', l_loss_train )
+for l_epoch in range(l_n_epochs):
+    print('training epoch #' + str(l_epoch+1))
+    l_loss_train = eml.mlp.trainer.train(l_loss_func,
+                                         l_data_loader_train,
+                                         l_model,
+                                         l_optimizer)
+    print('  training loss:', l_loss_train)
 
-  l_loss_test, l_n_correct_test = eml.mlp.tester.test( l_loss_func,
-                                                       l_data_loader_test,
-                                                       l_model )
-  l_accuracy_test = l_n_correct_test / len(l_data_loader_test.dataset)
-  print( '  test loss:', l_loss_test )
-  print( '  test accuracy:', l_accuracy_test )
+    l_loss_test, l_n_correct_test = eml.mlp.tester.test(l_loss_func,
+                                                        l_data_loader_test,
+                                                        l_model)
+    l_accuracy_test = l_n_correct_test / len(l_data_loader_test.dataset)
+    print('  test loss:', l_loss_test)
+    print('  test accuracy:', l_accuracy_test)
 
-  # visualize results of intermediate model every 10 epochs
-  if( (l_epoch+1) % 10 == 0 ):
-    l_file_name =  'test_dataset_epoch_' + str(l_epoch+1) + '.pdf'
-    print( '  visualizing intermediate model w.r.t. test dataset: ' + l_file_name )
-    eml.vis.fashion_mnist.plot( 0,
-                                250,
-                                l_data_loader_test,
-                                l_model,
-                                l_file_name )
+# ------------------- 7.2 Addon ------------------- #
+# Could be called only for rank 0, but lets ignore it :)
+
+    # visualize results of intermediate model every 100 epochs
+    # if((l_epoch+1) % 100 == 0):
+    #     l_file_name = 'test_dataset_epoch_' + str(l_epoch+1) + '.pdf'
+    #     print('  visualizing intermediate model w.r.t. test dataset: ' + l_file_name)
+    #     eml.vis.fashion_mnist.plot(0,
+    #                                250,
+    #                                l_data_loader_test,
+    #                                l_model,
+    #                                l_file_name)
+
 
 # visualize results of final model
-l_file_name = 'test_dataset_final.pdf'
-print( 'visualizing final model w.r.t. test dataset:', l_file_name )
-eml.vis.fashion_mnist.plot( 0,
-                            250,
-                            l_data_loader_test,
-                            l_model,
-                            l_file_name )
+# l_file_name = 'test_dataset_final.pdf'
+# print('visualizing final model w.r.t. test dataset:', l_file_name)
+# eml.vis.fashion_mnist.plot(0,
+#                            250,
+#                            l_data_loader_test,
+#                            l_model,
+#                            l_file_name)
+# ------------------------------------------------- #
 
-print( "#############" )
-print( "# Finished! #" )
-print( "#############" )
+print("#############")
+print("# Finished! #")
+print("#############")
